4a8efc4a24977e04706f930631addb982bbc3021
Samat <Shaiykov7@mail.ru>
2021-06-24 13:05:05 +0500

    first commit

---
+++ backend/postcontroller.js
@@ -0,0 +1,49 @@
+const Post = require("./Post.js");
+const PostService = require("./postService.js");
+
+class PostController {
+    async create(req, res) {
+        try {
+            const post = await PostService.create(req.body, req.files.picture)
+            res.json(post)
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+
+    async getAll(req, res) {
+        try {
+            const posts = await PostService.getAll();
+            return res.json(posts);
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+    async getOne(req, res) {
+        try {
+            const post = await PostService.getOne(req.params.id)
+            return res.json(post)
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+    async update(req, res) {
+        try {
+            const updatedPost = await PostService.update(req.body);
+            return res.json(updatedPost);
+        } catch (e) {
+            res.status(500).json(e.message)
+        }
+    }
+    async delete(req, res) {
+        try {
+            const post = await PostService.create(req.params.id);
+            return res.json(post)
+        } catch (e) {
+            res.status(500).json(e)
+        }
+    }
+}
+
+
+exports.smth =  new PostController();
